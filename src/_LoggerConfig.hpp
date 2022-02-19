874aa398f55213c509cb4d3be3b42c6a6b5efc76
RVeips <rihardsveips@gmail.com>
2022-02-19 23:16:25 +0200

    ref

--- src/_LoggerConfig.hpp
+++ src/_LoggerConfig.hpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Hardware Module <https://github.com/CFXS/CFXS-Hardware>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -29,11 +29,11 @@ namespace CFXS::HW {
 
     private:
         static constexpr bool ENABLED                   = true;
-        static constexpr const char* LOG_PREFIX         = __DEBUG_TERMINAL_RESET_COLOR__ "CFXS-HW| ";
-        static constexpr const char* WARNING_PREFIX     = __DEBUG_TERMINAL_WARNING_COLOR__ "CFXS-HW| ";
-        static constexpr const char* ERROR_PREFIX       = __DEBUG_TERMINAL_ERROR_COLOR__ "CFXS-HW| ";
-        static constexpr const char* CONSTRUCTOR_PREFIX = __ANSI_CYAN__ "CFXS-HW| ";
-        static constexpr const char* DESTRUCTOR_PREFIX  = __ANSI_CYAN__ "CFXS-HW| ";
+        static constexpr const char* LOG_PREFIX         = DEBUG_TERMINAL_RESET_COLOR "CFXS-HW| ";
+        static constexpr const char* WARNING_PREFIX     = DEBUG_TERMINAL_WARNING_COLOR "CFXS-HW| ";
+        static constexpr const char* ERROR_PREFIX       = DEBUG_TERMINAL_ERROR_COLOR "CFXS-HW| ";
+        static constexpr const char* CONSTRUCTOR_PREFIX = ANSI_CYAN "CFXS-HW| ";
+        static constexpr const char* DESTRUCTOR_PREFIX  = ANSI_CYAN "CFXS-HW| ";
 
         HardwareLogger_Base() = default;
     };
